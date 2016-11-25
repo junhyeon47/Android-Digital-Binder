70a0dac210f5aeee9928a378ce51f92cad552fbb
Jun Hyeon KIM <junhyeon2@naver.com>
2016-11-25 09:45:01 +0900

    [Main] 레이아웃 변경 및 이미지 버튼 추가하고 이벤트 구현

--- app/src/main/java/team/code/effect/digitalbinder/main/MainActivity.java
+++ app/src/main/java/team/code/effect/digitalbinder/main/MainActivity.java
@@ -1,20 +1,44 @@
 package team.code.effect.digitalbinder.main;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Toast;
 
 import team.code.effect.digitalbinder.R;
+import team.code.effect.digitalbinder.camera.CameraActivity;
+import team.code.effect.digitalbinder.explorer.ExplorerActivity;
+import team.code.effect.digitalbinder.photobook.PhotobookActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     static final int EVENT_MIN_DELTA_X = 150;
     private float oldEventX, newEventX;
+    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void btnClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_camera:
+                intent = new Intent(MainActivity.this, CameraActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_explorer:
+                intent = new Intent(MainActivity.this, ExplorerActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_photobook:
+                intent = new Intent(MainActivity.this, PhotobookActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 
     @Override
