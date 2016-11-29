ee3e2a19d8158dc5740b7a1807d2e1f254f523fa
이경민 <kt7224@gmail.com>
2016-11-29 15:53:40 +0900

    블루투스 권한 수정

---
+++ app/src/main/java/team/code/effect/digitalbinder/main/BluetoothActivity.java
@@ -0,0 +1,89 @@
+package team.code.effect.digitalbinder.main;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.PersistableBundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.List;
+
+import team.code.effect.digitalbinder.R;
+
+public class BluetoothActivity extends ActionBarActivity{
+
+    public static  final int DISCOVER_DURATION = 300;
+    public static final int REQUEST_BLU = 1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth);
+    }
+
+    public void sendViaBluetooth(View v){
+
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(btAdapter == null){
+            Toast.makeText(this, "이 기기는 블루투스를 지원하지 않습니다. ", Toast.LENGTH_SHORT).show();
+        }else {
+            enableBluetooth();
+        }
+    }
+
+    public void enableBluetooth() {
+
+        Intent discoveryIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+
+        discoveryIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, DISCOVER_DURATION);
+
+        startActivityForResult(discoveryIntent, REQUEST_BLU);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode==DISCOVER_DURATION && requestCode == REQUEST_BLU){
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            File f = new File(Environment.getExternalStorageDirectory(), "md5sum.txt");
+            intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(f));
+
+            PackageManager pm=getPackageManager();
+
+            List<ResolveInfo> appsList=pm.queryIntentActivities(intent, 0);
+
+            if(appsList.size() > 0){
+                String packageName = null;
+                String className = null;
+                boolean found = false;
+
+                for(ResolveInfo info : appsList){
+                    packageName = info.activityInfo.packageName;
+                    if(packageName.equals("com.android.bluetooth")){
+                        className=info.activityInfo.name;
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found){
+                    Toast.makeText(this, "블루투스가 발견되지 않았습니다.", Toast.LENGTH_SHORT).show();
+                }else {
+                    intent.setClassName(packageName, className);
+                    startActivity(intent);
+                }
+            }
+        }else {
+            Toast.makeText(this, "블루투스가 취소되었습니다.", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+}
--- app/src/main/java/team/code/effect/digitalbinder/main/MainActivity.java
+++ app/src/main/java/team/code/effect/digitalbinder/main/MainActivity.java
@@ -5,15 +5,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.PaintDrawable;
+import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,52 +27,41 @@ import android.view.animation.TranslateAnimation;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+
 
 import team.code.effect.digitalbinder.R;
 import team.code.effect.digitalbinder.camera.CameraActivity;
 import team.code.effect.digitalbinder.explorer.ExplorerActivity;
 import team.code.effect.digitalbinder.photobook.PhotobookActivity;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+
     static final int PERMISSION_CAMERA = 1;
     Intent intent;
-
-    //메뉴가 표시되어 있음을 나타냄
-    boolean alreadyShowing = false;
-    //현재창 너비
-    int windowWidth;
-    //현재창 높이
-    int windowHeight;
-    //화면을 흐리게
-    PopupWindow fadePopup;
-    //애니메이션 전환
-    Animation ta;
-    //전환할때 필요한 뷰
-    RelativeLayout baseView;
-    //LayoutInflater의 레퍼런스
-    LayoutInflater inflater;
+    MenuItem item;
 
     static final int EVENT_MIN_DELTA_X = 150;
     float oldEventX, newEventX;
+
+    //마지막으로 뒤로가기 버튼이 터치된 시간
+    private long lastTimeBackPressed;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        DisplayMetrics disp = getApplicationContext().getResources().getDisplayMetrics();
-        windowWidth = disp.widthPixels;
-        windowHeight = disp.heightPixels;
-        inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
     }
 
     public void btnClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.btn_camera:
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                     checkCameraPermssion();
-                }else{
+                } else {
                     intent = new Intent(MainActivity.this, CameraActivity.class);
                     startActivity(intent);
                 }
@@ -85,101 +79,31 @@ public class MainActivity extends AppCompatActivity{
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()){
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 oldEventX = event.getX();
                 break;
             case MotionEvent.ACTION_UP:
                 newEventX = event.getX();
                 float deltaX = newEventX - oldEventX;
-                if (deltaX > EVENT_MIN_DELTA_X)
-                {
-                    Toast.makeText(this, "swipe", Toast.LENGTH_SHORT).show ();
-                    if(!alreadyShowing){
-                        alreadyShowing = true;
-                        openSlidingMenu();
-                    }
+                if (deltaX > EVENT_MIN_DELTA_X) {
+                    Toast.makeText(this, "swipe", Toast.LENGTH_SHORT).show();
+
                 }
                 break;
         }
         return true;
     }
-    /*전체 화면을 흐리게 처리하고 배경이 희미 해집니다.*/
-    public void showFadePopup() {
-        final View layout = inflater.inflate(R.layout.item_fadepopup, (ViewGroup) findViewById(R.id.fadePopup));
-        fadePopup = new PopupWindow(layout, windowWidth, windowHeight, false);
-        fadePopup.showAtLocation(layout, Gravity.NO_GRAVITY, 0, 0);
-    }
-    /*슬라이딩 메뉴 열기*/
-    public void openSlidingMenu() {
-        showFadePopup();
-        // 메뉴의 너비
-        int width = (int) (windowWidth * 0.6);
-        translateView((float) (width));
-        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
-        // 팝업창 만들기
-        final View layout = inflater.inflate(R.layout.item_popup,(ViewGroup) findViewById(R.id.popup_element));
 
-        final PopupWindow optionsPopup = new PopupWindow(layout, width, height, true);
-        optionsPopup.setBackgroundDrawable(new PaintDrawable());
 
-        optionsPopup.showAtLocation(layout, Gravity.NO_GRAVITY, 0, 0);
-
-        optionsPopup.setOnDismissListener(new PopupWindow.OnDismissListener() {
-
-            public void onDismiss() {
-                //페이드 효과 제거
-                fadePopup.dismiss();
-                //애니메이션 전환 전 지우기
-                cleanUp();
-                //뷰 전환
-                translateView(0);
-                //애니메이션 전환 지우기
-                cleanUp();
-                //변수 재설정
-                alreadyShowing = false;
-            }
-        });
+    public void btnClick() {
+        Toast.makeText(this, "클릭했어?", Toast.LENGTH_SHORT).show();
     }
 
-    /**
-     이 메서드는 뷰 변환을 담당합니다.
-     */
-    public void translateView(float right) {
-
-        ta = new TranslateAnimation(0f, right, 0f, 0f);
-        ta.setDuration(300);
-        ta.setFillEnabled(true);
-        ta.setFillAfter(true);
-
-        //baseView =  (RelativeLayout) findViewById(R.id.baseView);
-        //baseView.startAnimation(ta);
-        //baseView.setVisibility(View.VISIBLE);
-    }
-
-    /**
-     메모리 문제를 피하기위한 기본 정리
-     */
-    public void cleanUp(){
-        if (null != baseView) {
-            baseView.clearAnimation();
-            baseView = null;
-        }
-        if (null != ta) {
-            ta.cancel();
-            ta = null;
-        }
-        fadePopup = null;
-    }
-
-    public void btnClick(){
-        Toast.makeText(this, "클릭했어?", Toast.LENGTH_SHORT).show ();
-    }
-
-    public void checkCameraPermssion(){
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+    public void checkCameraPermssion() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_CAMERA);
-        }else{
+        } else {
             intent = new Intent(MainActivity.this, CameraActivity.class);
             startActivity(intent);
         }
@@ -187,12 +111,29 @@ public class MainActivity extends AppCompatActivity{
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
+        switch (requestCode) {
             case PERMISSION_CAMERA:
-                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     intent = new Intent(MainActivity.this, CameraActivity.class);
                     startActivity(intent);
                 }
         }
+    }
+    /*뒤로가기 두번 누르면 종료*/
+    @Override
+    public void onBackPressed() {
+        //1.5초 내로 뒤로가기 버튼을 또 터치했으면 앱을 종료한다.
+        if(System.currentTimeMillis() - lastTimeBackPressed < 1500){
+            finish();
+            return;
+        }
+        Toast.makeText(this, "'뒤로' 버튼을 한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
+        //뒤로가기 버튼을 터치할 때마다 현재시간을 기록해둔다.
+        lastTimeBackPressed =System.currentTimeMillis();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
