d30f98a841abfc1edab6e2c3ba7bf0dd046c786d
Jun Hyeon KIM <junhyeon2@naver.com>
2016-12-01 15:51:59 +0900

    [camera] Camera1 구현 완료. Camera2  API로도 구현 해야함

--- app/src/main/java/team/code/effect/digitalbinder/common/AppConstans.java
+++ app/src/main/java/team/code/effect/digitalbinder/common/AppConstans.java
@@ -7,11 +7,11 @@ import android.os.Environment;
  */
 
 public class AppConstans {
-    public static  final String DIR=Environment.getExternalStorageDirectory().getAbsolutePath();
-    public static final String APP_PATH =DIR+"/DigitalBinder";
+    public static final String DIR = Environment.getExternalStorageDirectory().getAbsolutePath();
+    public static final String APP_PATH = DIR + "/DigitalBinder";
     public static final String DB_PATH = "/data/data/team.code.effect.digitalbinder/databases/digitalBinder.sqlite";
-    public static  final String BLUETOOTH="블루투스";
-    public static  final String NFC="NFC";
-    public static  final String GDRIVE="Google Drive";
-    public static  final String NDRIVE="N Drive";
+    public static final String BLUETOOTH = "블루투스";
+    public static final String NFC = "NFC";
+    public static final String GDRIVE = "Google Drive";
+    public static final String NDRIVE = "N Drive";
 }
