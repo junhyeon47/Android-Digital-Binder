6b3662fa2f24f860df613fdd3d91e56f16b8c2b3
Jun Hyeon KIM <junhyeon2@naver.com>
2016-12-01 17:43:00 +0900

    [Main] CloudActivity 툴바 설정 완료.

--- app/src/main/java/team/code/effect/digitalbinder/main/CloudActivity.java
+++ app/src/main/java/team/code/effect/digitalbinder/main/CloudActivity.java
@@ -1,5 +1,6 @@
 package team.code.effect.digitalbinder.main;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -15,12 +16,20 @@ public class CloudActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cloud);
-
         toolbar = (Toolbar)findViewById(R.id.toolbar); //XML 툴바의 주소를 toolbar로 연결.
         setSupportActionBar(toolbar); //툴바를 현재 액티비티의 액션바로 설정.
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //툴바에 뒤로가기 버튼 추가.
+        setToolbar(); //툴바의 설정을 변경하는 메소드 호출.
+
     }
 
+    //툴바의 설정을 변경하는 메소드
+    public void setToolbar(){
+        getSupportActionBar().setTitle("클라우드로 내보내기");
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFFF2594B)); //툴바색상 변경
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //툴바에 뒤로가기 버튼 추가.
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_white_24dp); //뒤로가기 버튼 아이콘 변경
+    }
+    
     @Override
     public boolean onOptionsItemSelected(MenuItem item) { //툴바의 메뉴 터치 이벤트
         switch (item.getItemId()){
