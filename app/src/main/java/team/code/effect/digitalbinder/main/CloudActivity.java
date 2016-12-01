4a42122ca54b92c3eeaaffb3fbe1caa463e62f6a
Jun Hyeon KIM <junhyeon2@naver.com>
2016-12-01 16:54:02 +0900

    [Main] CloudActivty 추가로 매니페이스트 수정

--- app/src/main/java/team/code/effect/digitalbinder/main/CloudActivity.java
+++ app/src/main/java/team/code/effect/digitalbinder/main/CloudActivity.java
@@ -1,8 +1,10 @@
 package team.code.effect.digitalbinder.main;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by 1238TX on 2016-11-30.
  */
 
-public class CloudActivity {
+public class CloudActivity extends AppCompatActivity{
 }
