7b23d8702a18adc109c3301e4dca1fbb06cafcf6
Jun Hyeon KIM <junhyeon2@naver.com>
2016-12-02 11:09:54 +0900

    [Main] 권한 수정!

--- app/src/main/java/team/code/effect/digitalbinder/main/MainActivity.java
+++ app/src/main/java/team/code/effect/digitalbinder/main/MainActivity.java
@@ -11,7 +11,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 
@@ -26,28 +25,21 @@ import team.code.effect.digitalbinder.common.DatabaseHelper;
 import team.code.effect.digitalbinder.explorer.ExplorerActivity;
 import team.code.effect.digitalbinder.photobook.PhotobookActivity;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     static final int PERMISSION_USING_CAMERA = 1;
-    NavigationView navigationView;
-    Intent intent;
-    MenuItem item;
+    static final int PERMISSION_USING_EXPLORER = 2;
+    static final int PERMISSION_USING_PHOTOBOOK = 3;
+    private Intent intent;
+    private long lastTimeBackPressed; //마지막으로 뒤로가기 버튼이 터치된 시간
+    public  static BinderDAO dao; //DAO 선언
 
-
-    static final int EVENT_MIN_DELTA_X = 150;
-    float oldEventX, newEventX;
-
-    //마지막으로 뒤로가기 버튼이 터치된 시간
-    private long lastTimeBackPressed;
-
-    //DAO 선언
-    public  static BinderDAO dao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         checkFolder();
         //메뉴 리스너
-        navigationView = (NavigationView)findViewById(R.id.nav_view);
+        NavigationView navigationView = (NavigationView)findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         //데이터베이스 얻어오기
@@ -65,38 +57,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 }
                 break;
             case R.id.btn_explorer:
-                intent = new Intent(MainActivity.this, ExplorerActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.btn_photobook:
-                intent = new Intent(MainActivity.this, PhotobookActivity.class);
-                startActivity(intent);
-                break;
-
-        }
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                oldEventX = event.getX();
-                break;
-            case MotionEvent.ACTION_UP:
-                newEventX = event.getX();
-                float deltaX = newEventX - oldEventX;
-                if (deltaX > EVENT_MIN_DELTA_X) {
-                    Toast.makeText(this, "swipe", Toast.LENGTH_SHORT).show();
-
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    checkExplorerPermission();
+                } else {
+                    intent = new Intent(MainActivity.this, ExplorerActivity.class);
+                    startActivity(intent);
                 }
                 break;
+            case R.id.btn_photobook:
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    checkPhotobookPermission();
+                } else {
+                    intent = new Intent(MainActivity.this, PhotobookActivity.class);
+                    startActivity(intent);
+                }
+                break;
+
         }
-        return true;
-    }
-
-
-    public void btnClick() {
-        Toast.makeText(this, "클릭했어?", Toast.LENGTH_SHORT).show();
     }
 
     public void checkCameraPermssion() {
@@ -113,6 +90,30 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
+    public void checkExplorerPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_USING_EXPLORER);
+        } else {
+            intent = new Intent(MainActivity.this, ExplorerActivity.class);
+            startActivity(intent);
+        }
+    }
+    public void checkPhotobookPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_USING_PHOTOBOOK);
+        } else {
+            intent = new Intent(MainActivity.this, PhotobookActivity.class);
+            startActivity(intent);
+        }
+    }
+
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
@@ -121,6 +122,19 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     intent = new Intent(MainActivity.this, CameraActivity.class);
                     startActivity(intent);
                 }
+                break;
+            case PERMISSION_USING_EXPLORER:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    intent = new Intent(MainActivity.this, ExplorerActivity.class);
+                    startActivity(intent);
+                }
+                break;
+            case PERMISSION_USING_PHOTOBOOK:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    intent = new Intent(MainActivity.this, PhotobookActivity.class);
+                    startActivity(intent);
+                }
+                break;
         }
     }
     /*뒤로가기 두번 누르면 종료*/
@@ -134,11 +148,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toast.makeText(this, "'뒤로' 버튼을 한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
         //뒤로가기 버튼을 터치할 때마다 현재시간을 기록해둔다.
         lastTimeBackPressed =System.currentTimeMillis();
-    }
-
-    public void moreSend(){
-        Toast toast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
-        toast.setView(getLayoutInflater().inflate(R.layout.activity_bluetooth, null));
     }
 
     @Override
