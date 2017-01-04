64f2012dc9284a0c6b8df5ada3779bf813289d4f
Junhyeon KIM <junhyeon2@naver.com>
2017-01-04 21:12:14 +0900

    [main] 구글 로그인 API 적용

--- app/src/main/java/team/code/effect/digitalbinder/main/MainActivity.java
+++ app/src/main/java/team/code/effect/digitalbinder/main/MainActivity.java
@@ -4,42 +4,77 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Typeface;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-
-import java.io.File;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import team.code.effect.digitalbinder.R;
 import team.code.effect.digitalbinder.camera.CameraActivity;
-import team.code.effect.digitalbinder.common.AppConstans;
 import team.code.effect.digitalbinder.common.PhotobookDAO;
 import team.code.effect.digitalbinder.common.DatabaseHelper;
 import team.code.effect.digitalbinder.explorer.ExplorerActivity;
 import team.code.effect.digitalbinder.photobook.PhotobookListActivity;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    static final int PERMISSION_USING_CAMERA = 1;
-    static final int PERMISSION_USING_EXPLORER = 2;
-    static final int PERMISSION_USING_PHOTOBOOK = 3;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private static final String TAG = "MainActivity";
+    private static final int PERMISSION_USING_CAMERA = 1;
+    private static final int PERMISSION_USING_EXPLORER = 2;
+    private static final int PERMISSION_USING_PHOTOBOOK = 3;
+    private static final int RC_SIGN_IN = 9001;
     private Intent intent;
     private long lastTimeBackPressed; //마지막으로 뒤로가기 버튼이 터치된 시간
     public static SQLiteDatabase db;
     public static PhotobookDAO dao; //DAO 선언
+    private TextView mLoginMessageTextView; //로그인 메세지
+    private TextView mEmailTextView;
+    private TextView mDisplayNameTextView;
+    private MenuItem mSignOutMenuItem;
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
+    // [START declare_auth_listener]
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    // [END declare_auth_listener]
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        checkFolder();
+
         //메뉴 리스너
         NavigationView navigationView = (NavigationView)findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -47,6 +82,48 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         //데이터베이스 얻어오기
         db = DatabaseHelper.initialize(this);
         dao = new PhotobookDAO(MainActivity.db);
+
+        mLoginMessageTextView = (TextView)navigationView.getHeaderView(0).findViewById(R.id.tv_login_message);
+        mEmailTextView = (TextView) navigationView.getHeaderView(0).findViewById(R.id.tv_email);
+        mDisplayNameTextView = (TextView) navigationView.getHeaderView(0).findViewById(R.id.tv_display_name);
+        mSignOutMenuItem = navigationView.getMenu().findItem(R.id.nav_sign_out);
+        // [START config_signin]
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // [END config_signin]
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        // [START initialize_auth]
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+
+        // [START auth_state_listener]
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // [START_EXCLUDE]
+                updateUI(user);
+                // [END_EXCLUDE]
+            }
+        };
+        // [END auth_state_listener]
+
+        signIn();
     }
 
     public void btnClick(View view) {
@@ -75,7 +152,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                     startActivity(intent);
                 }
                 break;
-
+            case R.id.tv_login_message:
+                signIn();
+                break;
         }
     }
 
@@ -156,24 +235,148 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
-            case R.id.nav_share:
-                intent = new Intent(MainActivity.this, SendActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.nav_info:
+            case R.id.nav_settings:
                 intent = new Intent(MainActivity.this, InfoActivity.class);
                 startActivity(intent);
-                break;
+                return true;
+            case R.id.nav_sign_out:
+                signOut();
+                return true;
         }
         return false;
     }
-    // App 폴더 있는지 확인
-    public void checkFolder() {
-        File dir = new File(AppConstans.APP_PATH);
-        if (dir.exists() == false) {
-            if (dir.mkdir()) {
-            }
-        } else {
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "onConnected called");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended called");
+    }
+
+    // [START on_start_add_listener]
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    // [END on_start_add_listener]
+
+    // [START on_stop_remove_listener]
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
         }
+    }
+    // [END on_stop_remove_listener]
+
+    // [START onactivityresult]
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+                mLoginMessageTextView.setVisibility(View.GONE);
+                mEmailTextView.setVisibility(View.VISIBLE);
+                mDisplayNameTextView.setVisibility(View.VISIBLE);
+                mEmailTextView.setText(account.getEmail());
+                mDisplayNameTextView.setText(account.getDisplayName());
+                mDisplayNameTextView.setTypeface(null, Typeface.BOLD);
+                mSignOutMenuItem.setVisible(true);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                // [START_EXCLUDE]
+                updateUI(null);
+                // [END_EXCLUDE]
+            }
+        }
+    }
+    // [END onactivityresult]
+
+    // [START auth_with_google]
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    // [END auth_with_google]
+
+    // [START signin]
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    // [END signin]
+
+    private void signOut() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google sign out
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        updateUI(null);
+                        Log.d(TAG, status.getStatus().toString());
+                        if(status.getStatusCode() == CommonStatusCodes.SUCCESS){
+                            mDisplayNameTextView.setVisibility(View.GONE);
+                            mEmailTextView.setVisibility(View.GONE);
+                            mLoginMessageTextView.setVisibility(View.VISIBLE);
+                            mSignOutMenuItem.setVisible(false);
+                        }
+                    }
+                });
+    }
+
+    private void revokeAccess() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google revoke access
+        Auth.GoogleSignInApi.revokeAccess(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        updateUI(null);
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
     }
 }
